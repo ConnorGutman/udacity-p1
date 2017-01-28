98a8199a5e782981438dc6b1483d646c347e43e6
connorgutman <connorgutman@gmail.com>
2017-01-28 13:11:04 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Fresh Tomatoes
+
+Fresh Tomatoes is a online database of Connor Gutman's favorite movies.
+
+### Table of contents
+  - [Getting Started](#getting-started)
+  - [Contributors](#contributors)
+  - [Notes](#notes)
+
+### Getting Started
+
+* Make sure you have Python installed.
+* Obtain a copy of this project:
+Clone the repo: `git clone https://github.com/ConnorGutman/project1.git`
+* Change into the project's directory.
+* run `python entertainment_center.py`
+
+### Contributors
+**Connor Gutman**
+- <https://connor.io/>
+- <https://github.com/ConnorGutman>
+
+**Udacity**
+- Thank you for `fresh_tomatoes.py`
+
+### Notes
+These are just a **few** of my favorite films!
---
+++ media.py
@@ -0,0 +1,23 @@
+# Import necessary dependencies such as webbrowser
+import webbrowser
+
+# Create a class called Movie which will contain relevant information
+# about films
+
+
+class Movie():
+    """This class provides a way to store movie information"""
+    # Create the initializer for the Movie class. This will declare several
+    # variables shown bellow such as title, storyline, etc.
+
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    # Create a function called show_trailer which opens a movie's trailer in
+    # your web browser.
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
